11c923db18a0a85b3d6bb37afb713f96960f51a0
Martin Schinz <martin.schinz@pebblecode.com>
2014-06-06 12:10:39 +0100

    add script, packages and first failing test

---
+++ test/FizzBuzzTests/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace FizzBuzzTests
+
+type Class1() = 
+    member this.X = "F#"
---
+++ test/FizzBuzzTests/Script.fsx
@@ -0,0 +1,13 @@
+﻿#I "../../packages"
+#r "Unquote.2.2.2/lib/net40/Unquote.dll"
+
+open System
+open Swensen.Unquote
+
+let fizzBuzz (num : int) =
+    ()
+//    if num > 100 then
+//        raise (ArgumentOutOfRangeException("num", num, "number must be less than or equal to 100"))
+
+
+raises<ArgumentOutOfRangeException> <@ fizzBuzz 101 @>
