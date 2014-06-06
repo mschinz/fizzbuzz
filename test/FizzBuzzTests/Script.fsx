988cac4e0af3df27a0878fc738ee669c16cce10d
Martin Schinz <martin.schinz@pebblecode.com>
2014-06-06 12:24:17 +0100

    add second failure

--- test/FizzBuzzTests/Script.fsx
+++ test/FizzBuzzTests/Script.fsx
@@ -5,9 +5,11 @@ open System
 open Swensen.Unquote
 
 let fizzBuzz (num : int) =
-    ()
-//    if num > 100 then
-//        raise (ArgumentOutOfRangeException("num", num, "number must be less than or equal to 100"))
+    if num > 100 then
+        raise (ArgumentOutOfRangeException("num", num, "number must be less than or equal to 100"))
+    if num < 1 then
+        raise (ArgumentOutOfRangeException("num", num, "number must be greater than or equal to 1"))
 
 
 raises<ArgumentOutOfRangeException> <@ fizzBuzz 101 @>
+raises<ArgumentOutOfRangeException> <@ fizzBuzz 0 @>
